74f3e25e5d9b151945ff34cc6c38eb4390b45d4c
Müller Nocciolli Ribeiro <muller.nocciolli@gmail.com>
2025-01-02 17:40:55 -0300

    Feat: Criação do banco de dados

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,14 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+
+
+class Task(models.Model):
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    task = models.CharField(max_length=100)
+
+
+    def __str__(self):
+
+        return self.task
